5caa278870080203dd0bc4609809106e33e36b6e
Juan Segura <jsegura8@horizon.csueastbay.edu>
2016-11-26 15:38:02 -0800

    Add files via upload

---
+++ portfolio/stylesheet/style-custom.php
@@ -0,0 +1,301 @@
+/* Background
+   ================================= */
+
+/* Logo
+   ================================= */
+.logo-wrapper{
+	margin-top: 50px;
+	margin-left: 10px;
+	margin-bottom: 33px;
+}
+.navigation-wrapper{
+	margin-top: 51px;
+}
+
+/* Font Size
+   ================================= */
+h1{
+	font-size: 39px;
+}
+h2{
+	font-size: 30px;
+}
+h3{
+	font-size: 25px;
+}
+h4{
+	font-size: 20px;
+}
+h5{
+	font-size: 18px;
+}
+h6{
+	font-size: 16px;
+}
+
+/* Element Color
+   ================================= */
+
+html{
+	background-color: #FCFCFC;
+}
+div.social-icon, /* to fix IE problem */
+div.container{
+	background: #FCFCFC;
+}
+div.divider{
+	border-bottom: 1px solid #ececec;
+}
+
+/* Font Family
+  ================================= */
+body{
+	font-family: default -;
+}
+h1, h2, h3, h4, h5, h6, .gdl-title{
+	font-family: default -;
+}
+.stunning-text-wrapper{
+	background-color: #ffffff !important;
+	border-bottom: 1px solid #dddddd;
+}
+h1.stunning-text-title{
+	font-family: default -;
+	color: #333333;
+}
+.gdl-slider-title{
+	font-family: default -;
+}
+.stunning-text-caption{
+	color: #8c8c8c;
+}
+
+/* Font Color
+   ================================= */
+body{
+	color: #919191 !important;
+}
+.footer-wrapper a{
+	color: #000000;
+}
+a{
+	color: #171717;
+}
+.gdl-link-title{
+	color: #171717 !important;
+}
+a:hover{
+	color: #a1a1a1;
+}
+.footer-wrapper a:hover{
+	color: #a8a8a8;
+}
+h1, h2, h3, h4, h5, h6, .title-color{
+	color: #171717;
+}
+div.gdl-page-title-left-bar{
+	background-color: #e3e3e3;
+}
+div.gdl-page-caption{
+	color: #7d7d7d;
+}
+.sidebar-title-color{
+	color: #171717 !important;
+}
+
+/* Slider Color
+	================================ */
+.gdl-slider-title{
+	color: #ffffff !important;
+}
+.gdl-slider-caption, .nivo-caption{
+		color: #c6c6c6 !important;
+}
+.flex-control-nav li a span,
+.nivo-controlNav a span,
+div.anythingSlider .anythingControls ul a span{
+	background-color: #c5c5c5 !important;
+}
+.nivo-controlNav a:hover span,
+.nivo-controlNav a.active span,
+.flex-control-nav li a:hover span,
+.flex-control-nav li a.active span,
+div.anythingSlider .anythingControls ul a:hover span,
+div.anythingSlider .anythingControls ul a.cur span{
+	background-color: #4e4e4e !important;
+}
+.flex-caption{
+	background-color: #000000 !important;
+}
+
+/* Post/Port Color
+   ================================= */
+.post-title-color{
+	color: #646464 !important;
+}
+.post-title-color a:hover{
+	color: #646464 !important;
+}
+div.single-port-little-bar{
+	border-color: #b4b4b4 !important;
+}
+.single-port-info span.head,
+.single-info-header,
+.blog-info-header{
+	color: #212121 !important;
+}
+.port-info-color, .port-info-color a,
+.post-info-color, .post-info-color a,
+div.custom-sidebar #twitter_update_list{
+	color: #9e9e9e !important;
+}
+div.pagination a{ background-color: #f9f9f9; }
+
+.about-author-wrapper{
+	background-color: #f9f9f9 !important;
+}
+.tagcloud a{
+	background-color: #fff !important;
+}
+div.footer-widget-wrapper .tagcloud a{
+	background-color: #fff !important;
+}
+
+#portfolio-item-filter a{
+	color: #a1a1a1;
+}
+#portfolio-item-filter a:hover{
+	color: #272727;
+}
+#portfolio-item-filter a.active{
+	color: #272727;
+}
+
+/* Column Service
+   ================================= */
+h2.column-service-title{
+	color: #3a3a3a !important;
+}
+
+
+
+/* Divider Color
+   ================================= */
+.scroll-top{
+	color: #7c7c7c !important;
+}
+.gdl-divider,
+.custom-sidebar.gdl-divider div,
+.custom-sidebar.gdl-divider .custom-sidebar-title,
+.custom-sidebar.gdl-divider ul li{
+	border-color: #ececec !important;
+}
+table th{
+	color: #666666;
+	background-color: #f7f7f7;
+}
+table, table tr, table tr td, table tr th{
+	border-color: #e5e5e5;
+}
+
+
+
+/* Tabs Color
+   ================================= */
+ul.tabs{
+	border-color: #dddddd !important;
+}
+ul.tabs li a {
+	color: #666666 !important;
+	background-color: #f5f5f5 !important;
+	border-color: #dddddd !important;
+}
+ul.tabs li a.active {
+	color: #111111 !important;
+	background-color: #ffffff !important;
+}
+
+/* Navigation Color
+   ================================= */
+.navigation-wrapper .sf-menu ul,
+.navigation-wrapper .sf-menu ul li{
+	border-color: #ececec !important;
+}
+.sf-menu li li{
+	background-color: #fdfdfd !important;
+}
+.navigation-wrapper .sf-menu li a{
+	color: #7d7d7d !important;
+}
+.navigation-wrapper .sf-menu ul a,
+.navigation-wrapper .sf-menu ul .current-menu-ancestor ul a,
+.navigation-wrapper .sf-menu ul .current-menu-item ul a,
+.navigation-wrapper .sf-menu .current-menu-ancestor ul a,
+.navigation-wrapper .sf-menu .current-menu-item ul a{
+	color: #7d7d7d !important;
+}
+.navigation-wrapper .sf-menu ul a:hover,
+.navigation-wrapper .sf-menu ul .current-menu-item ul a:hover,
+.navigation-wrapper .sf-menu .current-menu-item ul a:hover{
+	color: #343434 !important;
+}
+.navigation-wrapper .sf-menu a:hover,
+.navigation-wrapper .sf-menu a:active{
+	color: #171717 !important;
+}
+.navigation-wrapper .sf-menu .current-menu-ancestor a,
+.navigation-wrapper .sf-menu .current-menu-item a {
+	color: #171717 !important;
+}
+.navigation-wrapper .sf-menu ul .current-menu-ancestor a,
+.navigation-wrapper .sf-menu ul .current-menu-ancestor ul .current-menu-item a,
+.navigation-wrapper .sf-menu ul .current-menu-item a {
+	color: #343434 !important;
+}
+.search-wrapper{
+	border-left: 1px solid ;
+}
+.search-wrapper form{
+	border-left: 1px solid ;
+}
+
+
+
+/* Button Color
+   ================================= */
+a.button, button, input[type="submit"], input[type="reset"], input[type="button"],
+a.gdl-button{
+	background-color: #f1f1f1;
+	color: #7a7a7a;
+	border: 1px solid #dedede}
+
+a.button:hover, button:hover, input[type="submit"]:hover, input[type="reset"]:hover, input[type="button"]:hover,
+a.gdl-button:hover{
+	color: #7a7a7a;
+}
+
+
+
+
+/* Elements Shadow
+   ================================= */
+
+a.button, button, input[type="submit"], input[type="reset"], input[type="button"],
+a.gdl-button{
+	-moz-box-shadow: 1px 1px 3px #ececec;
+	-webkit-box-shadow: 1px 1px 3px #ececec;
+	box-shadow: 1px 1px 3px #ececec;
+}
+
+div.gdl-price-item .price-item.active{
+	-moz-box-shadow: 0px 0px 3px #ececec;
+	-webkit-box-shadow: 0px 0px 3px #ececec;
+	box-shadow: 0px 0px 3px #ececec;
+}
+
+div.stunning-text-corner{ width: 11px; height: 11px; position: absolute; }
+div.stunning-text-corner.top{ top: 0px; left: 0px; }
+div.stunning-text-corner.bottom{ background-position: -11px 0px; bottom: 0px; right: 0px; }
+
+div.column-service-learn-more{ font-style: italic; margin-top: 8px; }
+div#custom-full-background { z-index: -1; }
